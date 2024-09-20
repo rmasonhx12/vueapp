d200eeb97af88bec3d264cc9ca36ecaa7e7bfd6a
Robert Hendricks <rmasonh22@gmail.com>
2024-09-20 16:35:23 -0400

    first iteration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<p>Welcome to Vue {{name}} loves {{wife}} A/F!</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      name: 'Robert M Hendricks',
+      wife: 'Jacqueline T Hendricks'
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app
+{
+ 
 }
 </style>
